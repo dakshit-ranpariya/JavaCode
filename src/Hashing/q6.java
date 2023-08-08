d69d7bd3434aaae6eaea4de907af5917376f3331
dakshit10 <dakshitranpariya00211@gmail.com>
2023-08-08 16:55:45 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/Hashing/q6.java
@@ -0,0 +1,28 @@
+//Print all the duplicate numbers in array
+
+package Hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class q6 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2, 3, 4, 1, 7};
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for(int i:arr){
+            if(hashMap.containsKey(i)){
+                hashMap.put(i, hashMap.get(i)+1);
+            }else{
+                hashMap.put(i, 1);
+            }
+        }
+
+        for(Map.Entry me : hashMap.entrySet()){
+            if((int)me.getValue() > 1){
+                System.out.print(me.getKey() + " ");
+            }
+        }
+    }
+}
